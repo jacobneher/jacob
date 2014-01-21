53aee9f7abf99c13187bf9f620be490b0af3c2a8
Jacob Neher <jacob.neher@gmail.com>
2014-01-20 23:21:13 -0700

    First commit

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# C:\Ruby200-x64\lib\ruby\gems\2.0.0\gems\susy\lib\susy.rb
+require "/Ruby200-x64/lib/ruby/gems/2.0.0/gems/susy/lib/susy.rb"
